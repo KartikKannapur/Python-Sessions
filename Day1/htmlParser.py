6d61cbc799003ecabebe901798d8a16035d38663
abishekshenoy <abishekshenoy@gmail.com>
2014-04-13 23:50:07 +0530

    Create htmlParser.py

---
+++ Day1/htmlParser.py
@@ -0,0 +1,16 @@
+Opens a local HTML document and prints out tags+data
+
+
+import urllib
+feed=urllib.urlopen('file:///E:/Mtech%20Python%20Sessions/test.html')
+a=feed.read()
+
+from HTMLParser import HTMLParser
+
+class myParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print tag
+    def handle_endtag(self, tag):
+        print tag
+    def handle_data(self, data):
+        print data
