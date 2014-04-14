656deff397977911a85361b23629aec365655852
Kartik Kannapur <kartikkannapur@gmail.com>
2014-04-14 18:02:53 +0530

    Create lists.py

---
+++ Day2/lists.py
@@ -0,0 +1,34 @@
+#Lists 
+
+os = ["windows","ubuntu","fedora","Mountain Lion","openSUSE","linux mint","Debian"]
+
+android = ["Cupcake","Donut","Eclair","Froyo","Gingerbread","Honeycomb","Ice Cream Sandwich","Jelly Bean","Key Lime Pie"]
+
+numbers = [27,198,88,57,9,22,5]
+
+#Elements
+a = os[3]
+b = os[-4]
+c = os[-1]
+
+#Concatenation
+con = os + android
+
+#Duplicate
+dup = android * 3
+
+#length
+length = len(os)
+
+#Check
+check1 = "Donut" in android
+check2 = "Eclair" in os
+
+#Min and Max
+mi = min(numbers)
+ma = max(numbers)
+
+#List
+li = list('IEEE PESIT')
+
+
