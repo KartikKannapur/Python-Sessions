b33ee8cd4ebaa3a4bb4f7cf43546699a5fe66d0b
abishekshenoy <abishekshenoy@gmail.com>
2014-04-14 15:47:47 +0530

    Update ProjectEuler7.py

--- Day1/ProjectEuler7.py
+++ Day1/ProjectEuler7.py
@@ -9,10 +9,11 @@ in the range 3-sqrt(num). If so it is not prime!'''
 
 import math
 
+pos=500
 count=0 #to count primes
 num=3 #We start at 3, the second prime.
 
-while count<10000:  #10001-1 because 2 is excluded in prev step
+while count<pos-1:  #10001-1 because 2 is excluded in prev step
     
     flag=0
     for i in range(3,int(math.sqrt(num))+1): #range(3,int(num**.5)+1) - without use of math lib
@@ -22,6 +23,6 @@ while count<10000:  #10001-1 because 2 is excluded in prev step
     if flag==0:
         count=count+1
         
-    num=num+2 #check only odd numbers to save time
-    
+    num=num+2 #Check only odd numbers to save time
+
 print (num-2) #Remove extra 2 added at end before while loop was broken
