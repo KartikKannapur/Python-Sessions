98d291deb0225138889b0b92bfef11c896a3ed4c
Abishek Shenoy <abishekshenoy@gmail.com>
2014-04-16 14:53:12 +0530

    Update imageblend.py

--- Day2/imageblend.py
+++ Day2/imageblend.py
@@ -3,15 +3,13 @@
 '''pix,pix1 are pixel access objects ie. pointers to respective images
 Any changes made through them directly affect the respective image.'''
 
-import os
-import sys
 from PIL import Image
+
 
 row=[]
 col=[]
-
-pic1=Image.open("pic1.png").convert("L")
-pic=Image.open("pic.jpg").convert("L")
+pic1=Image.open("pic1.jpg").convert("L")
+pic=Image.open("pic.png").convert("L")
 
 pix=pic.load()
 pix1=pic1.load()
@@ -19,34 +17,14 @@ pix1=pic1.load()
 p1size=pic1.size
 psize=pic.size
 
-for i in range(psize[1]):
-    for j in range(psize[0]):
-        row.append(pix[j,i])
-    col.append(row)
-    row=[]
-
-'''for i in range(len(col)):
-        for j in range(psize[0]):
-                if col[i][j]<50:
-                        col[i][j]=0
-                else:
-                        col[i][j]=255'''
-                                                    
-
-for i in range(psize[1]):
-    for j in range(psize[0]):
-        pix[j,i]=col[i][j]
-
 respic=pic.resize((p1size[0],p1size[1]), Image.NEAREST)
 respix=respic.load()
 
-for i in range(p1size[1]):
-    for j in range(p1size[0]):
-        pix1[j,i]=pix1[j,i]+respix[j,i]
+blend=Image.blend(pic1,pic,0.5)
         
 
-pic1.show()
-pic1.save('blended.jpg')
+blend.show()
+blend.save('blend.jpg')
 
         
     
